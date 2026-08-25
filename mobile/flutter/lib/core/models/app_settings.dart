fb7929b16bb6710261b0710d8a7a541e8c62ba25
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-26 00:21:29 +0300

    Replace backend settings with standalone camera settings

--- mobile/flutter/lib/core/models/app_settings.dart
+++ mobile/flutter/lib/core/models/app_settings.dart
@@ -1,52 +1,69 @@
+enum CameraSourceType { phone, esp32 }
+
+CameraSourceType cameraSourceFromStorage(String? value) {
+  return switch (value) {
+    'esp32' => CameraSourceType.esp32,
+    _ => CameraSourceType.phone,
+  };
+}
+
+extension CameraSourceTypeStorage on CameraSourceType {
+  String get storageValue => this == CameraSourceType.esp32 ? 'esp32' : 'phone';
+}
+
 class AppSettings {
   const AppSettings({
+    this.firstRunCompleted = false,
     this.languageCode = 'en-US',
+    this.cameraSource = CameraSourceType.phone,
     this.speechEnabled = true,
     this.vibrationEnabled = true,
-    this.edgeHost = '192.168.4.2',
-    this.edgePort = 9999,
-    this.edgeMode = true,
+    this.esp32ListenPort = 9999,
     this.autoReconnect = true,
     this.detectionConfidence = 0.50,
     this.detectedClasses = const {
       'person',
-      'vehicle',
+      'car',
+      'truck',
+      'bus',
       'bicycle',
       'motorcycle',
-      'obstacle',
-      'stairs',
-      'curb',
+      'traffic light',
+      'stop sign',
+      'chair',
+      'bench',
+      'potted plant',
     },
   });
 
+  final bool firstRunCompleted;
   final String languageCode;
+  final CameraSourceType cameraSource;
   final bool speechEnabled;
   final bool vibrationEnabled;
-  final String edgeHost;
-  final int edgePort;
-  final bool edgeMode;
+  final int esp32ListenPort;
   final bool autoReconnect;
   final double detectionConfidence;
   final Set<String> detectedClasses;
 
   AppSettings copyWith({
+    bool? firstRunCompleted,
     String? languageCode,
+    CameraSourceType? cameraSource,
     bool? speechEnabled,
     bool? vibrationEnabled,
-    String? edgeHost,
-    int? edgePort,
-    bool? edgeMode,
+    int? esp32ListenPort,
     bool? autoReconnect,
     double? detectionConfidence,
     Set<String>? detectedClasses,
   }) {
     return AppSettings(
+      firstRunCompleted: firstRunCompleted ?? this.firstRunCompleted,
       languageCode: languageCode ?? this.languageCode,
+      cameraSource: cameraSource ?? this.cameraSource,
       speechEnabled: speechEnabled ?? this.speechEnabled,
       vibrationEnabled: vibrationEnabled ?? this.vibrationEnabled,
-      edgeHost: edgeHost ?? this.edgeHost,
-      edgePort: edgePort ?? this.edgePort,
-      edgeMode: edgeMode ?? this.edgeMode,
+      esp32ListenPort: esp32ListenPort ?? this.esp32ListenPort,
       autoReconnect: autoReconnect ?? this.autoReconnect,
       detectionConfidence: detectionConfidence ?? this.detectionConfidence,
       detectedClasses: detectedClasses ?? this.detectedClasses,
